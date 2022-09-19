fa15690d316cbf20bbae1fbe9ecbded18bfdbf94
techprab <69295906+techprab@users.noreply.github.com>
2022-09-20 00:04:44 +0530

    first commit by Prabjot

---
+++ app/src/main/java/com/hackathon/floodreleif/MainActivity.java
@@ -0,0 +1,14 @@
+package com.hackathon.floodreleif;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_screen_part_two);
+    }
+}
